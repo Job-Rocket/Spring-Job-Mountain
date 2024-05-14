afde59f16ca4619a095a39d7f2c6da1f8afefd19
jisoo <yangjs229@gmail.com>
2024-05-14 16:44:07 +0900

    Fix: nginx

--- src/main/java/com/example/job_mountain/config/IndexController.java
+++ src/main/java/com/example/job_mountain/config/IndexController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/health")
 public class IndexController {
 
     @GetMapping
