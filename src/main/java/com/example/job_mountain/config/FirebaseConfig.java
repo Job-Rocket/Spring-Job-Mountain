a697109552bb9d2978443a6f9a2c33ce6822a757
jisoo <yangjs229@gmail.com>
2024-05-26 15:19:25 +0900

    Fix: Firebase

--- src/main/java/com/example/job_mountain/config/FirebaseConfig.java
+++ src/main/java/com/example/job_mountain/config/FirebaseConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 
 @Configuration
@@ -18,11 +18,11 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        // Firebase 설정 파일을 가져옵니다.
+        // Firebase 설정 파일을 클래스패스에서 읽습니다.
         ClassPathResource serviceAccountFile = new ClassPathResource("jobdongsani-bfeeb-firebase-adminsdk-wsen9-e1808536bb.json");
 
         // Gson을 사용하여 lenient 모드로 JSON을 읽기
-        try (JsonReader reader = new JsonReader(new FileReader(serviceAccountFile.getFile()))) {
+        try (JsonReader reader = new JsonReader(new InputStreamReader(serviceAccountFile.getInputStream()))) {
             reader.setLenient(true);
             JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
 
