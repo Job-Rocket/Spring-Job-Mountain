69071e7432c5c8cae65a664ab2dc18c534c8c17c
jisoo <yangjs229@gmail.com>
2024-05-26 15:22:41 +0900

    Fix: Firebase

--- src/main/java/com/example/job_mountain/config/FirebaseConfig.java
+++ src/main/java/com/example/job_mountain/config/FirebaseConfig.java
@@ -3,14 +3,9 @@ package com.example.job_mountain.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import java.io.InputStreamReader;
 import java.io.IOException;
 
 @Configuration
@@ -18,23 +13,15 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        // Firebase 설정 파일을 클래스패스에서 읽습니다.
+        // Firebase 설정 파일을 가져옵니다.
         ClassPathResource serviceAccountFile = new ClassPathResource("jobdongsani-bfeeb-firebase-adminsdk-wsen9-e1808536bb.json");
 
-        // Gson을 사용하여 lenient 모드로 JSON을 읽기
-        try (JsonReader reader = new JsonReader(new InputStreamReader(serviceAccountFile.getInputStream()))) {
-            reader.setLenient(true);
-            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+        // FirebaseOptions를 설정합니다.
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
+                .build();
 
-            // FirebaseOptions를 설정합니다.
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
-                    .setProjectId(jsonObject.get("project_id").getAsString())
-                    .setDatabaseUrl(jsonObject.get("database_url").getAsString())
-                    .build();
-
-            // FirebaseApp을 초기화하고 반환합니다.
-            return FirebaseApp.initializeApp(options);
-        }
+        // FirebaseApp을 초기화하고 반환합니다.
+        return FirebaseApp.initializeApp(options);
     }
 }
