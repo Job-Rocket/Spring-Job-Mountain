7f0c86ca713aa5a1d73fbd7df6607873a84443ab
jisoo <yangjs229@gmail.com>
2024-05-26 15:13:59 +0900

    Fix: Firebase

--- src/main/java/com/example/job_mountain/config/FirebaseConfig.java
+++ src/main/java/com/example/job_mountain/config/FirebaseConfig.java
@@ -3,9 +3,14 @@ package com.example.job_mountain.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileReader;
 import java.io.IOException;
 
 @Configuration
@@ -16,12 +21,20 @@ public class FirebaseConfig {
         // Firebase 설정 파일을 가져옵니다.
         ClassPathResource serviceAccountFile = new ClassPathResource("jobdongsani-bfeeb-firebase-adminsdk-wsen9-e1808536bb.json");
 
-        // FirebaseOptions를 설정합니다.
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
-                .build();
+        // Gson을 사용하여 lenient 모드로 JSON을 읽기
+        try (JsonReader reader = new JsonReader(new FileReader(serviceAccountFile.getFile()))) {
+            reader.setLenient(true);
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
 
-        // FirebaseApp을 초기화하고 반환합니다.
-        return FirebaseApp.initializeApp(options);
+            // FirebaseOptions를 설정합니다.
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
+                    .setProjectId(jsonObject.get("project_id").getAsString())
+                    .setDatabaseUrl(jsonObject.get("database_url").getAsString())
+                    .build();
+
+            // FirebaseApp을 초기화하고 반환합니다.
+            return FirebaseApp.initializeApp(options);
+        }
     }
 }
