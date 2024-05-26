65ba8969cc979c2d8adc9fe94866b8e593ac3003
jisoo <yangjs229@gmail.com>
2024-05-26 15:53:13 +0900

    Fix: Firebase

--- src/main/java/com/example/job_mountain/config/FirebaseConfig.java
+++ src/main/java/com/example/job_mountain/config/FirebaseConfig.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 @Configuration
@@ -26,15 +28,20 @@ public class FirebaseConfig {
         String jsonString;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(serviceAccountFile.getInputStream()))) {
             jsonString = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-            System.out.println(jsonString);
+            System.out.println(jsonString); // JSON 내용 출력
         }
 
         // Gson을 사용하여 JSON 문자열을 파싱합니다.
         JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
 
+        // JSON 객체에서 필요한 키가 존재하는지 확인
+        if (!jsonObject.has("project_id") || !jsonObject.has("database_url")) {
+            throw new IllegalArgumentException("Invalid Firebase configuration: missing required keys");
+        }
+
         // FirebaseOptions를 설정합니다.
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile.getInputStream()))
+                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))))
                 .setProjectId(jsonObject.get("project_id").getAsString())
                 .setDatabaseUrl(jsonObject.get("database_url").getAsString())
                 .build();
